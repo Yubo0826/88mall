b00437062469fd76514443e15c368a8b75b24d64
Yubo0826 <ujhy669917@gmail.com>
2022-01-16 20:01:33 +0800

    部屬測試

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/88mall/'
+//       : './'
+//   }
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/88mall/'
-      : './'
-  }
+    publicPath: './'
+}
